7250ce508be5911a1dd1961fd120df6ee6ba01c8
Yevhen Stepiuk <stepyuk.android@gmail.com>
2015-10-10 19:42:58 +0300

    Added add button

--- app/src/main/java/com/example/user/oshack/CreateConvRoot.java
+++ app/src/main/java/com/example/user/oshack/CreateConvRoot.java
@@ -7,6 +7,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -31,6 +33,9 @@ public class CreateConvRoot extends Activity {
     public static class AddTasksFragment extends Fragment {
 
         private ListView listView;
+        private Button addButton;
+        private EditText addTaskEditor;
+
         private ArrayAdapter<String> adapter;
         private ArrayList<String> tasks = new ArrayList<>();
 
@@ -44,11 +49,24 @@ public class CreateConvRoot extends Activity {
             super.onViewCreated(view, savedInstanceState);
 
             listView = (ListView) view.findViewById(R.id.list_view);
+            addButton = (Button) view.findViewById(R.id.add_button);
+            addTaskEditor = (EditText) view.findViewById(R.id.new_task);
+
             adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, tasks);
 
             listView.setAdapter(adapter);
-            tasks.add("Hello");
-            adapter.notifyDataSetChanged();
+
+            addButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if (addTaskEditor.getText().toString().equals("")) {
+                        return;
+                    }
+                    tasks.add(addTaskEditor.getText().toString());
+                    adapter.notifyDataSetChanged();
+                }
+            });
         }
 
     }
