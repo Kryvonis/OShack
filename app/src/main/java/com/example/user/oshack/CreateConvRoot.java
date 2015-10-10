0fb0fee698c4b52b56446f52b74a5c8055d84b08
Kryvonis <kryvonis.artem@gmail.com>
2015-10-10 17:15:46 +0300

    create layout

--- app/src/main/java/com/example/user/oshack/CreateConvRoot.java
+++ app/src/main/java/com/example/user/oshack/CreateConvRoot.java
@@ -1,7 +1,10 @@
 package com.example.user.oshack;
 
+import android.app.Activity;
+
 /**
  * Created by user on 10/10/15.
  */
-public class CreateConvRoot {
+public class CreateConvRoot extends Activity {
+
 }
